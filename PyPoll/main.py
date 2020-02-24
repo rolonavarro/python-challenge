0b7481c6871ebc6517c430ae1783cc4ea39d654f
rolonavarro <59516844+rolonavarro@users.noreply.github.com>
2020-02-24 14:57:26 -0600

    Update main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,34 @@
 
+import os
+import csv
+
+file = os.path.join(...)
+
+def percent(Candidate_Vote, TotVote)
+  return ((Cadidate_Vote / TotVote) * 100)
+
+TotVote = 0
+kcount = 0
+ccount = 0
+lcount = 0
+ocount = 0
+
+with open(file) as csvpoll:
+  csvreader = csv.reader(csvpoll, delimeter=',')
+  
+  for row in csvreader:
+    voterid = row[0]
+    county = row[1]
+    candidate = row[2]
+    
+    if candidate == "Khan":
+      kcount = kcount + 1
+    
+    if candidate == "Correy":
+      ccount = ccount + 1
+     
+    if candidate == "Li":
+      lcount = lcount + 1
+      
+    if candidate == "O'Tooley":
+      ocount = ocount + 1
